01938c743c1064fc675a550f8e8bda17b0491e2a
github username <github email address>
2017-12-01 15:48:41 -0500

    preliminary combination of two components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 // import Calendar from "./datepicker.js"
 import MyMap from "./MyMap.js"
-import Calendar from "./datepicker.js"
+import HeatmapCalendar from "./CalendarHeatmap.js"
 class App extends Component {
   render() {
     return (
       <div>
         <MyMap />
-         <Calendar />
+         <HeatmapCalendar />
       </div>
     );
   }
