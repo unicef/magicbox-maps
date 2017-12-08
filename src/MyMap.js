ec19ec4d311b1cefce7ce1c23b7c786d4f2707cb
mikefab <mikefabrikant@gmail.com>
2017-12-08 18:17:29 -0500

    Fix errors on methods

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,56 +1,86 @@
-import React, {Component} from 'react'
-import allGeojson  from './data/allCountriesGEOJSON.js'
+import allGeojson from './data/allCountriesGEOJSON.js'
 import displayCountries from './data/displayCountries.js';
-
 
 import {
   GeoJSON,
   Map,
   ZoomControl,
-  TileLayer,
+  TileLayer
 } from 'react-leaflet'
 // import L from 'leaflet';
 import {
   alpha3ToAlpha2
 } from 'i18n-iso-countries';
-var _ = require('lodash');
+const _ = require('lodash');
 
+/**
+ * My map class
+ */
 class MyMap extends Component {
-  state = {
-    lat: 0,
-    lng: 0,
-    zoom: 2,
+  /**
+   * State object
+   * @param  {object} props
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: 'https://api.tiles.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}.png?' +
+      'access_token=' +
+      'pk.eyJ1IjoiYXlhbmV6IiwiYSI6ImNqNHloOXAweTFveWwzM3A4M3FkOWUzM2UifQ.' +
+      'GfClkT4QxlFDC_xiI37x3Q',
+      attribution: '&copy; <a href=\'http://osm.org/copyright\'>OpenStreetMap</a>' +
+      ' contributors ',
+      lat: 0,
+      lng: 0,
+      zoom: 2
+    }
   }
-  countryStyle = (geoJsonFeature) => {
-    //var layer = e.target;
+
+  /**
+   * Returns style for leaflet polygon
+   * @param  {object} geoJsonFeature request object
+   * @return {object} style object
+   */
+  countryStyle(geoJsonFeature) {
+    // var layer = e.target;
     const displayCountry = {
       fill: true,
-      fillColor: "#0099FF",
+      fillColor: '#0099FF',
       fillOpacity: .5,
-      stroke: false,
+      stroke: false
     }
     const nullDisplay = {
       fill: false,
-      fillColor: "#0099FF",
+      fillColor: '#0099FF',
       fillOpacity: 0,
-      stroke: false,
+      stroke: false
     }
-    var alpha2 =alpha3ToAlpha2(geoJsonFeature.id);
+    let alpha2 = alpha3ToAlpha2(geoJsonFeature.id);
     if (displayCountries.indexOf(alpha2) > -1) {
       return displayCountry;
-    }else{
+    } else {
       return nullDisplay;
-
     }
   }
-  geoFilter = (feature) => {
-    var alpha2 = alpha3ToAlpha2(feature.id);
+
+  /**
+   * Returns style for leaflet polygon
+   * @param  {object} feature request object
+   * @return {boolean} boolean
+   */
+  geoFilter(feature) {
+    let alpha2 = alpha3ToAlpha2(feature.id);
     if (displayCountries.indexOf(alpha2) > -1) {
       return true
     }
     return false
   }
-  onEachFeature = (feature, layer) => {
+  /**
+   * onEach
+   * @param  {object} feature
+   * @param  {object} layer
+   */
+  onEachFeature(feature, layer) {
     // if (this.props.countrySelected.admin1 === feature.id) {
     //   layer.setStyle({
     //     fillOpacity: 0
@@ -61,18 +91,16 @@ class MyMap extends Component {
         layer.setStyle({
           fillOpacity: 0.7
         });
-
       },
       'mouseout': (e) => {
         layer.setStyle({
           fillOpacity: 0.5
         })
-
-
       },
       'click': (e) => {
         console.log(e);
-        if (this.props.countrySelected.admin1 !== feature.id && this.props.countrySelected.country === 'BR') {
+        if (this.props.countrySelected.admin1 !== feature.id &&
+          this.props.countrySelected.country === 'BR') {
           // var admin1 = e.target.feature.id;
           // //console.log(e.target.feature.properties.UF);
           // var admin1L = e.target.feature.properties.UF
@@ -84,22 +112,25 @@ class MyMap extends Component {
           // this.props.clickedCountry(alpha2, this.props.sliderData.value, admin1, admin1L);
           // toLoad = true;
         }
-
-
-
-        //console.log(e);
+        // console.log(e);
       }
     });
   }
-
+  /**
+   * Render
+   * @return {object} JSX
+   */
   render() {
     const position = [this.state.lat, this.state.lng]
     return (
-      <Map ref="map" center={position} zoom={this.state.zoom} zoomControl={false}>
-        <ZoomControl position="bottomleft" />
+      <Map ref='map'
+        center={position}
+        zoom={this.state.zoom}
+        zoomControl={false}>
+        <ZoomControl position='bottomleft' />
         <TileLayer
-          url='https://api.tiles.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYXlhbmV6IiwiYSI6ImNqNHloOXAweTFveWwzM3A4M3FkOWUzM2UifQ.GfClkT4QxlFDC_xiI37x3Q'
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors '
+          url={this.state.url}
+          attribution={this.state.attribution}
         />
         <GeoJSON
           key={_.uniqueId()}
@@ -109,7 +140,6 @@ class MyMap extends Component {
           filter={this.geoFilter.bind(this)}
         ></GeoJSON>
       </Map>
-
     )
   }
 }
