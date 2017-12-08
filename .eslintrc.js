c1ee0865ef0a8a844a8259b3144a6e4a25ac7c06
mikefab <mikefabrikant@gmail.com>
2017-12-08 16:17:57 -0500

    Precommit lint hook

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'parserOptions': {
+    'sourceType': 'module',
+    "ecmaFeatures": {
+        "jsx": true,
+        "modules": true
+    }
+  },
+    "extends": "google"
+};
