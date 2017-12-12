a2bfb7feb33701cefbbafd1cee00ae54eac0ff48
ayanez17 <fredyyanez39@yahoo.com>
2017-12-12 16:31:45 -0500

    Added Backend and dynamically displays countries based on API call

---
+++ src/reducers/index.jsx
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux';
+import datesReducer from './reducer-dates';
+import initialLoadReducer from './initialLoadReducer';
+
+const allReducers = combineReducers({
+  dates: datesReducer,
+  initialCountries: initialLoadReducer,
+
+})
+
+export default allReducers;
