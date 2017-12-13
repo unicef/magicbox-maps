3fe735dcf4d4b75e1dba1dc727c99372edccbed7
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-12-13 11:35:26 -0500

    Added map load fetch url to config

--- src/actions/initialLoad.jsx
+++ src/actions/initialLoad.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
+var config = require('../config.js')
 const InitialLoad = () => {
   // console.log("IN IL");
   return function(dispatch) {
-    axios.get(window.location.href +'/schools/countries' )
+    axios.get(window.location.href +config.init_map_url)
       .catch(err => {
         alert('There was an error trying to do the initial fetch')
       })
