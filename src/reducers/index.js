d9a133636b5947934d79115681e95eb465eaa4b3
mikefab <mikefabrikant@gmail.com>
2017-12-22 22:10:33 -0500

    Remove mobility reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,15 +6,13 @@ import dateReducer from './reducer-active-date';
 import allCountriesReducer from './reducer-all-countries';
 import initialLoadReducer from './initialLoadReducer';
 import activeCountryReducer from './reducer-active-country';
-import mobilityReducer from './reducer-mobility';
 
 const allReducers = combineReducers({
   activeCountry: activeCountryReducer,
   dates: datesReducer,
   date: dateReducer,
   allCountries: allCountriesReducer,
-  initialCountries: initialLoadReducer,
-  mobility: mobilityReducer
+  initialCountries: initialLoadReducer
 })
 
 export default allReducers;
