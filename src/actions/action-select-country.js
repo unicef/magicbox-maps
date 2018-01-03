88f73fa897fdc9b5f5112ff18341a1f63b895fd4
ayanez17 <fredyyanez39@yahoo.com>
2018-01-02 19:19:40 -0800

    when a country is selected the fnc makes an axios request

--- src/actions/action-select-country.js
+++ src/actions/action-select-country.js
@@ -1,13 +1,33 @@
+import arrToGeo from '../helpers/helper-2d-geojson'
+import axios from 'axios';
+const config = require('../config.js')
 /**
  * selectCountry - Specifies the style for the geojson
  *
  * @param  {String} country description
  * @return {object} style
  */
-export const selectCountry = country => {
+export const selectCountry = (country) => {
   console.log('You selected', country);
-  return {
-    type: 'COUNTRY_SELECTED',
-    payload: country
+  return function(dispatch) {
+    let geojson = null;
+    console.log(window.location.href + config.mode + '/countries/' + country);
+    axios.get(window.location.href + config.mode + '/countries/' + country)
+      .catch(err => {
+        alert('There was an error trying to do the initial fetch')
+      })
+      .then(response => {
+        const headersList = response.data.result[0];
+        const jsonData = response.data.result.slice(1)
+        geojson = arrToGeo(headersList, jsonData)
+        console.log('action', geojson);
+        dispatch({
+          type: 'COUNTRY_SELECTED',
+          payload: {
+            geojson: geojson,
+            selectedCountry: country
+          }
+        })
+      })
   }
 }
