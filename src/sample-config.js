1115358c091d7e711a78c8e51898038334098e44
mikefab <mikefabrikant@gmail.com>
2018-01-04 12:30:11 -0500

    Update config

--- src/sample-config.js
+++ src/sample-config.js
@@ -1,6 +1,11 @@
 // fill in appropriate variables and name config.js
 module.exports = {
-  mode: 'schools',
+  mode: 'schools', // or 'mobility'
+  initial_url_key: {
+    mobility: 'schools',
+    schools: 'schools'
+  },
+  login_required: false,
   magicbox_url: 'MAGICBOX_API_URL',
   rfUrl: 'REFRESH_TOKEN_URL',
   refreshToken: 'REFRESH_TOKEN'
