9d02b8818a69175690b2f5668f71f92e922114a6
ayanez17 <fredyyanez39@yahoo.com>
2018-01-06 14:34:10 -0800

    added axios call to fet weather at latlon

--- src/helpers/helper-country-point.js
+++ src/helpers/helper-country-point.js
@@ -1,5 +1,5 @@
 import L from 'leaflet';
-
+import axios from 'axios';
 /**
  * pointToLayer - makes point to layer
  *
@@ -8,11 +8,37 @@ import L from 'leaflet';
  * @return {type} circleMarker
  */
 export function pointToLayer(feature, latlng) {
-  return L.circleMarker(latlng, {
+  let marker = L.circleMarker(latlng, {
     color: 'white',
     fillColor: 'white',
     fillOpacity: .8,
     radius: 3,
     stroke: false
-  }).bindPopup('MESSAGE') // Change marker to circle
+  }).bindPopup('LOADING...') // Change marker to circle
+  marker.on('click', onDotClick)
+  return marker;
+}
+
+/**
+ * onMapClick - On click function for dot
+ *
+ * @param  {type} e event
+ */
+function onDotClick(e) {
+  console.log('in mapclick');
+  let popup = e.target.getPopup();
+  console.log(e.target.feature.geometry.coordinates);
+  let lat = e.target.feature.geometry.coordinates[1];
+  let lon = e.target.feature.geometry.coordinates[0];
+  console.log(window.location.origin + '/weather?lat=' + lat + '&lon=' + lon);
+  axios.get(window.location.origin + '/weather?lat=' + lat + '&lon=' + lon)
+    .catch(err => {
+      alert('There was an error trying to do the initial fetch')
+    })
+    .then(response => {
+      console.log(response);
+      popup.setContent(response.data.description)
+      popup.update();
+    })
+  console.log(popup);
 }
