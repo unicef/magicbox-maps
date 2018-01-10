99a024c56def6d7a0aeca4768a21d7077213d353
ayanez17 <fredyyanez39@yahoo.com>
2018-01-10 11:06:39 -0500

    creates pop up  message

---
+++ src/helpers/helper-popup-string.js
@@ -0,0 +1,18 @@
+/**
+ * popUpString - created pop up string
+ *
+ * @param {type} labels labels of data
+ * @param {type} data   actual data
+ *
+ * @return {type} returns string of labels
+ */
+const popUpString = (labels, data) => {
+  let output = '';
+  for (const i = 0; i < labels.length; i++) {
+    if (data[i] !== null) {
+      output += labels[i] + ': ' + data[i] + '<br>';
+    }
+  }
+  return output;
+}
+export default popUpString;
