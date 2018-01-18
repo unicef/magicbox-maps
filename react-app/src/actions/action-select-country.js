e14b0bea5809603e2b143f5ee1e1946aef2161d6
ayanez17 <fredyyanez39@yahoo.com>
2018-01-18 10:07:54 -0500

    ifcountry is clicked and it CO or BR, the docker shows up

--- react-app/src/actions/action-select-country.js
+++ react-app/src/actions/action-select-country.js
@@ -9,9 +9,9 @@ var iso3311a2 = require('iso-3166-1-alpha-2')
  * @param  {String} country description
  * @return {object} style
  */
-export const selectCountry = (country) => {
+export const selectCountry = (country, sliderVal) => {
   console.log('You selected', country);
-  if (mode != 'schools') {
+  if (mode !== 'schools') {
     return {
       type: 'COUNTRY_SELECTED',
       // Not used  as using mode now
@@ -25,21 +25,35 @@ export const selectCountry = (country) => {
         alert('There was an error trying to do the initial fetch')
       })
       .then(response => {
-        console.log(response);
+        // console.log(response);
         const headersList = response.data.result[0];
         const jsonData = response.data.result.slice(1)
         const points = arrToGeo(headersList, jsonData)
         const countryname = iso3311a2.getCountry(country)
         const numSchools = response.data.result.length - 1;
-        console.log(response.data.result[0]);
+        // console.log(response.data.result[0]);
         let nschools = 0;
         let speedschools = 0;
         let speedresult = null;
+        let belowT = 0;
+        let zeroT = 0;
+        let aboveT = 0;
+        let nullT = 0;
         for (let i = 1; i < response.data.result.length; i++) {
           if (response.data.result[i][3] !== null) {
             speedschools += response.data.result[i][3];
             nschools++;
+            if (response.data.result[i][3] == 0) {
+              zeroT++;
+            } else if (response.data.result[i][3] < sliderVal) {
+              belowT++;
+            } else if (response.data.result[i][3] >= sliderVal) {
+              aboveT++;
+            }
+          } else {
+            nullT++;
           }
+
         }
         if (speedschools === 0) {
           speedresult = null;
@@ -47,6 +61,7 @@ export const selectCountry = (country) => {
           speedresult = speedschools / nschools
           speedresult = speedresult.toFixed(2)
         }
+        //console.log(points);
         dispatch({
           type: 'COUNTRY_SELECTED',
           payload: {
@@ -57,6 +72,16 @@ export const selectCountry = (country) => {
             selectedCountryAvgMbps: speedresult
           }
         })
+        dispatch({
+          type: 'SLIDER_CHANGED',
+          payload: {
+            aboveT: aboveT,
+            belowT: belowT,
+            zeroT: zeroT,
+            nullT: nullT,
+          }
+        })
+
       })
   }
 }
