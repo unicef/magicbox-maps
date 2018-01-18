32c68f810d96cf94b101e72cfa7b210339039a33
ayanez17 <fredyyanez39@yahoo.com>
2018-01-18 10:11:52 -0500

    docker files

---
+++ react-app/src/actions/action-slider-change.js
@@ -0,0 +1,32 @@
+const SliderChange = function(geojson, sliderVal) {
+  return function(dispatch) {
+    let belowT = 0;
+    let zeroT = 0;
+    let aboveT = 0;
+    let nullT = 0;
+    for (let i = 0; i < geojson.features.length; i++) {
+      let speed = geojson.features[i].properties.speed_connectivity;
+      if (speed !== null) {
+        if (speed == 0) {
+          zeroT++;
+        } else if (speed < sliderVal) {
+          belowT++;
+        } else if (speed >= sliderVal) {
+          aboveT++;
+        }
+      } else {
+        nullT++;
+      }
+    }
+    dispatch({
+      type: 'SLIDER_CHANGED',
+      payload: {
+        aboveT: aboveT,
+        belowT: belowT,
+        zeroT: zeroT,
+        nullT: nullT,
+      }
+    })
+  }
+}
+export default SliderChange;
