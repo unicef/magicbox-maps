25b5feeaa7acdf6d6ab07ac2262ccdd3523c6bdd
mikefab <mikefabrikant@gmail.com>
2018-02-27 10:56:47 -0500

    update for callback

--- react-app/src/helpers/helper-country-point.js
+++ react-app/src/helpers/helper-country-point.js
@@ -38,36 +38,36 @@ export function assign_speed_value(properties) {
     return [1, 1, 1]
   }
 }
-
-/**
- * pointToLayer - makes point to layer
- *
- * @param  {Number} school_id
- * @param  {Obj} map
- */
-export function pointToLayer(school_id, map) {
-  let url = window.location.origin + '/' +
-    config.initial_url_key[mode] +
-    '/school/' + school_id;
-  console.log(url);
-  axios.get(url)
-    .catch(err => {
-      alert('There was an error trying to get school info')
-    })
-    .then(response => {
-      let labels = response.data.result[0];
-      let data = response.data.result[1]
-      let lat_index = labels.findIndex(function(e) {
-        return e.match(/^lat$/)
-      })
-      let lon_index = labels.findIndex(function(e) {
-        return e.match(/^lon$/)
-      })
-      let message = popUpString(labels, data)
-      window.aaa = labels;
-      window.bbb= data;
-      // console.log(message, 'Mmmm', lat_index, lon_index, data)
-      map.openPopup(message, [data[lat_index], data[lon_index]])
-    })
-  // console.log(popup);
-}
+//
+// /**
+//  * pointToLayer - makes point to layer
+//  *
+//  * @param  {Number} school_id
+//  * @param  {Obj} map
+//  */
+// export function pointToLayer(school_id, map) {
+//   let url = window.location.origin + '/' +
+//     config.initial_url_key[mode] +
+//     '/school/' + school_id;
+//   console.log(url);
+//   axios.get(url)
+//     .catch(err => {
+//       alert('There was an error trying to get school info')
+//     })
+//     .then(response => {
+//       let labels = response.data.result[0];
+//       let data = response.data.result[1]
+//       let lat_index = labels.findIndex(function(e) {
+//         return e.match(/^lat$/)
+//       })
+//       let lon_index = labels.findIndex(function(e) {
+//         return e.match(/^lon$/)
+//       })
+//       let message = popUpString(labels, data)
+//       window.aaa = labels;
+//       window.bbb= data;
+//       // console.log(message, 'Mmmm', lat_index, lon_index, data)
+//       map.openPopup(message, [data[lat_index], data[lon_index]])
+//     })
+//   // console.log(popup);
+// }
