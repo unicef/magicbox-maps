02b69fb90a283f7dc7c10c0b50c69a206a993f4d
mikefab <mikefabrikant@gmail.com>
2018-02-27 16:43:56 -0500

    import over require

--- react-app/src/reducers/reducer-active-country.js
+++ react-app/src/reducers/reducer-active-country.js
@@ -15,7 +15,7 @@ let mpio = {}
 let admin_index = {}
 
 if (config.mode !== 'schools') {
-  const mpio = require('../data/mpio');
+  mpio = import('../data/mpio');
 }
 // List of scores to represent mobility toward each admin
 // Order is by geoFeature index
