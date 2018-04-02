ff222d07c44fa4aa53b15fabea6f4d02e58c176c
Ryan DeBeasi <rdebeasi@redhat.com>
2018-04-02 10:33:56 -0400

    Use full school data set on Azure
    Retrieve data from a temporary Azure store so that we can use the full school data set, not just a few sample points. Eventually, this should be replaced with a longer-term storage solution.

--- src/helpers/api-config.js
+++ src/helpers/api-config.js
@@ -1,5 +1,8 @@
+// The school data is on a temporary Azure store and should be replaced
+// with a longer-term storage solution in the future.
+
 const apiConfig = {
-  schools: '/data/schools.json',
+  schools: 'https://teststorage3910.blob.core.windows.net/test/schools.json',
   shapes: '/data/mpio-hdi-pop-threats-violence.json'
 }
 
