d0a24bddb986663024e2dd2ca85f27ec3da47b52
Ryan DeBeasi <rdebeasi@redhat.com>
2018-04-06 15:11:11 -0400

    Move connectivity colors to a separate file

---
+++ src/helpers/connectivity-colors.js
@@ -0,0 +1,6 @@
+export default {
+  colorHigh: [92, 184, 92],
+  colorLow: [245, 166, 35],
+  colorNone: [217, 83, 79],
+  colorUnknown: [106, 30, 116],
+}
--- src/helpers/count-connectivity.js
+++ src/helpers/count-connectivity.js
@@ -1,13 +1,10 @@
+import connectivityColors from './connectivity-colors';
+
 function countConnectivity(schools) {
   let numHigh = 0; // Above 3Mbps
   let numLow = 0; // Below 3Mbps
   let numNone = 0; // No connectivity
   let numUnknown = 0;
-
-  let colorHigh = [92, 184, 92];
-  let colorLow = [245, 166, 35];
-  let colorNone = [217, 83, 79];
-  let colorUnknown = [106, 30, 116];
 
   // Currently, the school JSON only has colors, not the connectivity level.
   // In the future, consider placing the connectivity level into the
@@ -18,16 +15,16 @@ function countConnectivity(schools) {
 
   schools.forEach(school => {
     switch (school.properties.color[0]) {
-      case colorHigh[0]:
+      case connectivityColors.colorHigh[0]:
         numHigh++;
         break;
-      case colorLow[0]:
+      case connectivityColors.colorLow[0]:
         numLow++;
         break;
-      case colorNone[0]:
+      case connectivityColors.colorNone[0]:
         numNone++;
         break;
-      case colorUnknown[0]:
+      case connectivityColors.colorUnknown[0]:
       default:
         numUnknown++;
         break;
