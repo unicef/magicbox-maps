95a1e01cf4c953750eff48c7a553fac74475d3be
Vinit Singh <vinitveersingh@gmail.com>
2018-07-03 13:01:06 -0400

    #93 Fixed! Make API able to serve shapefiles for multiple countries

--- routes/shapefiles.js
+++ routes/shapefiles.js
@@ -2,9 +2,16 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-const mpio = require('../public/mpio.json');
-
 router.get('/:country', (req, res) => {
-  res.send(mpio)
+  _parsedUrl = req['_parsedUrl']
+  path = _parsedUrl['path']
+  console.log('path' + path)
+  if ( path === '/COD' ) {
+    const cod = require('../public/cod_2.json');
+    res.send(cod)
+  } else if ( path == '/COL' ) {
+    const mpio = require('../public/mpio.json');
+    res.send(mpio)
+  }
 })
 module.exports = router;
